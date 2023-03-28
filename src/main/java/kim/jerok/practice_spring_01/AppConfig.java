bc9ea55877fff16c0215d25737a0676f7883c4c5
jerok <jerok.kim@gmail.com>
2023-03-28 18:07:55 +0900

    practice spring

--- src/main/java/kim/jerok/practice_spring_01/AppConfig.java
+++ src/main/java/kim/jerok/practice_spring_01/AppConfig.java
@@ -1,30 +1,38 @@
 package kim.jerok.practice_spring_01;
 
 import kim.jerok.practice_spring_01.discount.DiscountPolicy;
-import kim.jerok.practice_spring_01.discount.FixDiscountPolicy;
+import kim.jerok.practice_spring_01.discount.RateDiscountPolicy;
 import kim.jerok.practice_spring_01.member.MemberRepository;
 import kim.jerok.practice_spring_01.member.MemberService;
 import kim.jerok.practice_spring_01.member.MemberServiceImpl;
 import kim.jerok.practice_spring_01.member.MemoryMemberRepository;
 import kim.jerok.practice_spring_01.order.OrderService;
 import kim.jerok.practice_spring_01.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-    }
-
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
+    
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+        // return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 
 }
--- src/main/java/kim/jerok/practice_spring_01/OrderApp.java
+++ src/main/java/kim/jerok/practice_spring_01/OrderApp.java
@@ -5,14 +5,20 @@ import kim.jerok.practice_spring_01.member.Member;
 import kim.jerok.practice_spring_01.member.MemberService;
 import kim.jerok.practice_spring_01.order.Order;
 import kim.jerok.practice_spring_01.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
-        
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+
+        // AppConfig appConfig = new AppConfig();
+        // MemberService memberService = appConfig.memberService();
+        // OrderService orderService = appConfig.orderService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
